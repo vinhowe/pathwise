07b9f9ed574fc2e7e2a716a5e230132a05cb40a8
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2021-04-07 03:49:35 -0600

    Add method to reconstruct path as list

--- src/main/kotlin/minecraft/McPath.kt
+++ src/main/kotlin/minecraft/McPath.kt
@@ -1,6 +1,7 @@
 package minecraft
 
 import Path
+import java.util.*
 
 class McPath(parents: MutableMap<McPathNode, McPathNode>, costs: MutableMap<McPathNode, Double>, var end: McPathNode) :
     Path<McPathNode>(parents, costs) {
@@ -8,5 +9,16 @@ class McPath(parents: MutableMap<McPathNode, McPathNode>, costs: MutableMap<McPa
         fun empty(end: McPathNode): McPath {
             return McPath(mutableMapOf(), mutableMapOf(), end)
         }
+    }
+
+    fun reconstruct(node: McPathNode = end): List<McPathNode> {
+        val path = Stack<McPathNode>()
+        path.push(node)
+        var current = node
+        while (current in parents) {
+            current = parents[current]!!
+            path.push(current)
+        }
+        return path.toList()
     }
 }
